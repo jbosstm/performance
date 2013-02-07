4b80909ac8e040ac1e8b56d4bf747792f6cfd6f6
mmusgrov <mmusgrov@redhat.com>
2013-02-07 17:59:04 +0000

    JBTM-1427 script the test

--- quickstart/narayana/ejb/perftest/eapcmp.sh
+++ quickstart/narayana/ejb/perftest/eapcmp.sh
@@ -1,12 +1,289 @@
 #!/bin/sh
 
+BASE_DIR=`pwd`
+RES_DIR=$BASE_DIR/results
+RES_FILE=$RES_DIR/perf.$$.tab
+EAP5_ZIP="http://download.devel.redhat.com/released/JBEAP-5/5.1.1/zip/jboss-eap-5.1.1.zip"
+EAP6_ZIP="http://download.devel.redhat.com/released/JBEAP-6/6.0.1/zip/jboss-eap-6.0.1.zip "
+PERF_REPO=https://github.com/jbosstm/performance
+PERF_REPO=https://github.com/mmusgrov/performance
+EAP5_DIR=$BASE_DIR/eap-5.1.1
+EAP6_DIR=$BASE_DIR/eap-6.0
+EAP5_WAIT=60
+EAP6_WAIT=20
+file_store=1
+jacorb_patch=0
+version="-"
+
+[ $JBOSS_TERM ] || JBOSS_TERM=gnome 
+
+server0_pid=0
+server1_pid=0
+
 function fatal {
     echo "fatal: $1"
-    if [ -f "$2" ]; then
-      cat $2
+    [ -f "$2" ] && cat $2
+
+    stop_servers
+    exit 1
+}
+
+function do_copy {
+  if [ -d $1 ]; then
+    cp -r $1 $2
+  else
+   cp $1 $2
+  fi
+
+  [ $? = 0 ] || fatal "do_copy: $1 $2 failed"
+}
+
+function configure_eap5 {
+  [ -d "$EAP5_DIR" ] || mkdir -p $EAP5_DIR
+  cd $EAP5_DIR
+
+  if [ ! -d jboss-eap-5.1 ]; then
+    if [ ! -f jboss-eap-5.1.1.zip ]; then
+      wget $EAP5_ZIP
+      [ $? = 0 ] || fatal "configure_eap5: wget $EAP5_ZIP failed"
+    fi
+    if [ ! -d jboss-eap-5.1/jboss-as/server/ ]; then
+      unzip jboss-eap-5.1.1.zip
+      [ $? = 0 ] || fatal "configure_eap5: unzip EAP5 failed"
+    fi
+    cd jboss-eap-5.1/jboss-as/server
+    do_copy all server0
+    do_copy all server1
+    cd ../docs/examples/transactions/
+    ant jts -Dtarget.server.dir=../../../server/server0
+    [ $? = 0 ] || fatal "enable EAP5 jts failed for server0"
+    ant jts -Dtarget.server.dir=../../../server/server1
+    [ $? = 0 ] || fatal "enable EAP5 jts failed for server1"
+    do_copy  $EAP5_DIR/jboss-eap-5.1/jboss-as/server/all/lib/jacorb.jar $BASE_DIR/performance/quickstart/narayana/ejb/perftest/jacorb.jar.eap5
+  fi
+}
+
+function configure_eap6 {
+  [ -d "$EAP6_DIR" ] || mkdir -p $EAP6_DIR
+  cd $EAP6_DIR
+
+  if [ ! -d jboss-eap-6.0 ]; then
+    if [ ! -f jboss-eap-6.0.1.zip ]; then
+      wget $EAP6_ZIP
+      [ $? = 0 ] || fatal "wget $EAP6_ZIP failed"
     fi
 
-    exit 1
+    unzip jboss-eap-6.0.1.zip
+    [ $? = 0 ] || fatal "unzip EAP6 failed"
+
+    do_copy jboss-eap-6.0 server0
+    do_copy jboss-eap-6.0 server1
+
+    do_copy  $EAP6_DIR/jboss-eap-6.0/modules/org/jacorb/main/jacorb-2.3.2-redhat-2.jar $BASE_DIR/performance/quickstart/narayana/ejb/perftest/jacorb.jar.eap6
+
+    enable_jts EAP6 0
+    enable_jts EAP6 1
+    stop_servers
+
+    do_copy  $EAP6_DIR/server0/standalone/configuration/standalone-full.xml $BASE_DIR/performance/quickstart/narayana/ejb/perftest/standalone-full-server0.xml
+    do_copy  $EAP6_DIR/server1/standalone/configuration/standalone-full.xml $BASE_DIR/performance/quickstart/narayana/ejb/perftest/standalone-full-server1.xml
+
+    enable_hornetq_os EAP6 0
+    enable_hornetq_os EAP6 1
+    stop_servers
+
+    do_copy  $EAP6_DIR/server0/standalone/configuration/standalone-full.xml $BASE_DIR/performance/quickstart/narayana/ejb/perftest/standalone-full-hq-server0.xml
+    do_copy  $EAP6_DIR/server1/standalone/configuration/standalone-full.xml $BASE_DIR/performance/quickstart/narayana/ejb/perftest/standalone-full-hq-server1.xml
+  fi
+}
+
+function clone_perf_repo {
+  if [ ! -d $BASE_DIR/performance/quickstart/narayana/ejb/perftest ]; then
+    cd $BASE_DIR
+    git clone $PERF_REPO
+    [ $? = 0 ] || fatal "clone performance repo failed"
+  fi
+}
+
+function update_ear {
+  cd $BASE_DIR/performance/quickstart/narayana/ejb/perftest
+  [ $? = 0 ] || fatal "perftest dir doesn't exist"
+
+  if [ ! -f perf-ear/target/perf-ear.ear ]; then
+    mvn clean install
+    [ $? = 0 ] || fatal "perftest clean install failed"
+  fi
+
+  do_copy perf-ear/target/perf-ear.ear $EAP5_DIR/jboss-eap-5.1/jboss-as/server/server0/deploy/perf-ear.ear
+  do_copy perf-ear/target/perf-ear.ear $EAP5_DIR/jboss-eap-5.1/jboss-as/server/server1/deploy/perf-ear.ear
+
+  do_copy perf-ear/target/perf-ear.ear $EAP6_DIR/server0/standalone/deployments/
+  do_copy perf-ear/target/perf-ear.ear $EAP6_DIR/server1/standalone/deployments/
+}
+
+function start_eap {
+  ver=$1
+  server=$2
+
+  if [ "$2" = "0" ]; then
+    port=8080
+    ports=default
+  elif [ "$2" = "1" ]; then
+    port=8180
+    ports=01
+  fi
+  if [ "$1" = "EAP5" ]; then
+    sleep=$EAP5_WAIT
+    bootcmd="$EAP5_DIR/jboss-eap-5.1/jboss-as/bin/run.sh -c server${2} -Djboss.messaging.ServerPeerID=${2} -Djboss.service.binding.set=ports-${ports}"
+  elif [ "$1" = "EAP6" ]; then
+    sleep=$EAP6_WAIT
+    bootcmd="$EAP6_DIR/server${2}/bin/standalone.sh -c standalone-full.xml -Djboss.socket.binding.port-offset=${2}00"
+  fi
+
+  # give time for any existing AS to shutdown
+  sleep 5
+  curl  http://localhost:$port > /dev/null 2>&1
+  [ $? = 0 ] && fatal "start_eap: server already running on port $port"
+
+  echo starting $1 server server$2 with command "$bootcmd"
+
+  if [ $JBOSS_TERM = "gnome" ]; then
+    gnome-terminal --disable-factory -x sh -c "$bootcmd" &
+  else
+    $bootcmd &
+  fi
+
+  if [ "$2" = "0" ]; then
+    server0_pid=$!
+  else
+    server1_pid=$!
+  fi
+
+  echo "server running with pid $!"
+  sleep $sleep
+}
+
+function stop_servers {
+  if [ $server0_pid != 0 ]; then
+    echo "killing $server0_pid"
+#    $EAP5_DIR/jboss-eap-5.1/jboss-as/bin/shutdown.sh -s localhost:1099 > /dev/null 2>&1
+#    $EAP6_DIR/server0/bin/jboss-cli.sh --connect command=:shutdown controller=localhost:9999 > /dev/null 2>&1
+    kill "$server0_pid"
+    server0_pid=0
+  fi
+  if [ $server1_pid != 0 ]; then
+    echo "killing $server1_pid"
+#    $EAP5_DIR/jboss-eap-5.1/jboss-as/bin/shutdown.sh -s localhost:1199 > /dev/null 2>&1
+#    $EAP6_DIR/server1/bin/jboss-cli.sh --connect command=:shutdown controller=localhost:10099 > /dev/null 2>&1
+    kill "$server1_pid"
+    server1_pid=0
+  fi
+}
+
+function enable_jts {
+  ver=$1
+  server=$2
+  port=9999
+
+  if [ "$1" = "EAP5" ]; then
+    return
+  fi
+
+  [ "$2" = "1" ] && port=10099
+
+  start_eap EAP6 $2
+
+  $EAP6_DIR/server$2/bin/add-user.sh admin adm1n
+
+  if  [ "$2" = "0" ]; then
+    $EAP6_DIR/server$2/bin/jboss-cli.sh --connect controller=localhost:9999 --user=admin --password=adm1n << JTS
+/subsystem=transactions/:write-attribute(name=jts,value=true)
+/subsystem=jacorb/:write-attribute(name=transactions,value=on)
+JTS
+#TODO /subsystem=transactions/:write-attribute(name=node-identifier,value=0)
+  else
+    $EAP6_DIR/server$2/bin/jboss-cli.sh --connect controller=localhost:10099 --user=admin --password=adm1n << JTS
+/subsystem=transactions/:write-attribute(name=jts,value=true)
+/subsystem=jacorb/:write-attribute(name=transactions,value=on)
+JTS
+#TODO jacorb.properties /subsystem=transactions/:write-attribute(name=node-identifier,value=1)
+  fi
+}
+    
+function enable_hornetq_os {
+  ver=$1
+  server=$2
+  port=9999
+
+  if [ "$ver" = "EAP5" ]; then
+    return
+  fi
+
+  [ "$server" = "1" ] && port=10099
+
+  start_eap EAP6 $server
+
+  if  [ "$server" = "0" ]; then
+      $EAP6_DIR/server${server}/bin/jboss-cli.sh --connect controller=localhost:9999 --user=admin --password=adm1n << JTS
+/subsystem=transactions/:write-attribute(name=use-hornetq-store,value=true)
+JTS
+  else
+      $EAP6_DIR/server${server}/bin/jboss-cli.sh --connect controller=localhost:10099 --user=admin --password=adm1n << JTS
+/subsystem=transactions/:write-attribute(name=use-hornetq-store,value=true)
+JTS
+  fi
+}
+
+function hornetq_os_enable {
+  do_copy $BASE_DIR/performance/quickstart/narayana/ejb/perftest/standalone-full-hq-server0.xml $EAP6_DIR/server0/standalone/configuration/standalone-full.xml
+  do_copy $BASE_DIR/performance/quickstart/narayana/ejb/perftest/standalone-full-hq-server1.xml $EAP6_DIR/server1/standalone/configuration/standalone-full.xml
+  file_store=0
+}
+
+function hornetq_os_disable {
+  do_copy $BASE_DIR/performance/quickstart/narayana/ejb/perftest/standalone-full-server0.xml $EAP6_DIR/server0/standalone/configuration/standalone-full.xml
+  do_copy $BASE_DIR/performance/quickstart/narayana/ejb/perftest/standalone-full-server1.xml $EAP6_DIR/server1/standalone/configuration/standalone-full.xml
+  file_store=1
+}
+
+function update_data_dir {
+  ver=$1
+  server=$2
+  enable=$3
+  mnt_dev=$4
+
+  ADATA_DIR="$mnt_dev/$ver/data$server"
+
+  if [ "$1" = "EAP5" ]; then
+    DATA_DIR="$EAP5_DIR/jboss-eap-5.1/jboss-as/server/server${2}/data"
+  elif [ "$1" = "EAP6" ]; then
+    DATA_DIR="$EAP6_DIR/server${2}/standalone/data"
+  fi
+
+  rm -rf "$DATA_DIR"
+  if [ "$enable" = 1 ]; then
+    [ -d "$ADATA_DIR" ] || mkdir -p $ADATA_DIR
+    rm -rf $ADATA_DIR/*
+    ln -s "$ADATA_DIR" "$DATA_DIR"
+  fi
+}
+
+function patch_jacorb {
+  if [ "$1" = "EAP5" ]; then
+    do_copy  $BASE_DIR/performance/quickstart/narayana/ejb/perftest/jacorb.jar.patched $EAP5_DIR/jboss-eap-5.1/jboss-as/server/server${2}/lib/jacorb.jar
+  else
+    do_copy  $BASE_DIR/performance/quickstart/narayana/ejb/perftest/jacorb.jar.patched $EAP6_DIR/server${2}/modules/org/jacorb/main/jacorb-2.3.2-redhat-2.jar
+  fi
+  jacorb_patch=1
+}
+
+function unpatch_jacorb {
+  if [ "$1" = "EAP5" ]; then
+    do_copy $BASE_DIR/performance/quickstart/narayana/ejb/perftest/jacorb.jar.eap5 $EAP5_DIR/jboss-eap-5.1/jboss-as/server/server${2}/lib/jacorb.jar
+  else
+    do_copy $BASE_DIR/performance/quickstart/narayana/ejb/perftest/jacorb.jar.eap6 $EAP6_DIR/server${2}/modules/org/jacorb/main/jacorb-2.3.2-redhat-2.jar
+  fi
+  jacorb_patch=0
 }
 
 function test {
@@ -40,38 +317,100 @@ function test {
         [ $? = 1 ] || fatal "run $i $1 $2" "res$i"; # failed
 
          v=$(cat res$i)
+# TODO assert v is a number
          tot=`expr $tot + $v`
     done
 
 #    throughput=`expr $tot / $threads`
     throughput=$tot
-    printf "%11s %8s %8s %13s %6s %6s\n" $throughput $calls $threads $transactional $enlist $remote
-#    echo $calls transactions $threads threads: throughput=$throughput
+    printf "%9s %11s %9s %9s %9s %9s %11s %9s %9s\n" $version $throughput $calls $jacorb_patch $file_store $threads $transactional $enlist $remote | tee -a $RES_FILE
 
     return 0
 }
 
-printf "%11s %8s %13s %8s %6s %6s\n" "Throughput" "Calls" "Threads" "Transactional" "Enlist" "Remote"
+function tests {
+  start_eap $1 0
+  start_eap $1 1
 
-case $# in
-0)
-   test 1000 1 0 0 0
-   test 1000 1 0 0 1
-   echo ""
-   test 10000 1 1 1 1
-   test 1000 10 1 1 1
-   echo ""
-   test 100 10 1 0 1
-   test 100 10 1 0 0
-   test 100 10 1 2 1
-   test 100 10 1 1 0
-   ;;
-1) test $1 3 1 1 1;;
-2) test $1 $2 1 1 1;;
-3) test $1 $2 $3 1 1;;
-4) test $1 $2 $3 $4 1;;
-*) test $1 $2 $3 $4 1;;
-esac
+  [ -d $RES_DIR ] || mkdir $RES_DIR
+  cd $RES_DIR
 
-#curl http://localhost:8080/perf-war/PerfTest -d "count=10&verbose=0" > /dev/null 2>&1
+  version=$1
+
+  if [ -f "$2" ]; then
+    cat $2 | while read ln; do
+      test $ln
+    done
+  else
+    test 1000 1 1 1 1
+    test 100 10 1 1 1
+  fi
+
+  stop_servers
+}
+
+function patch_eap {
+  patch_jacorb EAP5 0
+  patch_jacorb EAP6 0
+  patch_jacorb EAP5 1
+  patch_jacorb EAP6 1
+}
+
+function unpatch_eap {
+  unpatch_jacorb EAP5 0
+  unpatch_jacorb EAP6 0
+  unpatch_jacorb EAP5 1
+  unpatch_jacorb EAP6 1
+}
+
+function env_setup {
+  clone_perf_repo
+  configure_eap5
+  configure_eap6
+  update_ear
+}
+
+function unpatched_tests {
+  unpatch_eap
+  tests EAP6
+  tests EAP5
+}
+function patched_tests {
+  patch_eap
+  tests EAP6
+  tests EAP5
+}
+
+function change_data_dir {
+  update_data_dir EAP5 0 $1 "$2"
+  update_data_dir EAP5 1 $1 "$2"
+  update_data_dir EAP6 0 $1 "$2"
+  update_data_dir EAP6 1 $1 "$2"
+}
+
+env_setup
+
+printf "%9s %11s %9s %9s %9s %9s %9s %9s %9s\n" "Version" "Throughput" "Calls" "Patched" "FileStore" "Threads" "Transaction" "Enlist" "Remote" | tee -a $RES_FILE
+
+#change_data_dir 1 "/mnt"
+hornetq_os_disable
+unpatched_tests
+patched_tests
+
+hornetq_os_enable
+unpatched_tests
+patched_tests
+
+echo "Your results are awaiting your expert analysis in $RES_FILE"
 exit 0
+
+#   test 1000 1 0 0 0
+#   test 1000 1 0 0 1
+#   echo ""
+#   test 10000 1 1 1 1
+#   test 1000 10 1 1 1
+#   echo ""
+#   test 100 10 1 0 1
+#   test 100 10 1 0 0
+#   test 100 10 1 2 1
+#   test 100 10 1 1 0
