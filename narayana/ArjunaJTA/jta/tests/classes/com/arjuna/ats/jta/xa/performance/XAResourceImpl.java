ddd0f56e5b5709b9bbed1430e5095921b02108d1
Michael Musgrove <mmusgrov@redhat.com>
2014-08-06 17:04:55 +0100

    [JBTM-1453] OpenJDK: jmh testing

---
+++ narayana/ArjunaJTA/jta/tests/classes/com/arjuna/ats/jta/xa/performance/XAResourceImpl.java
@@ -0,0 +1,52 @@
+package com.arjuna.ats.jta.xa.performance;
+
+import javax.transaction.xa.XAException;
+import javax.transaction.xa.XAResource;
+import javax.transaction.xa.Xid;
+
+public class XAResourceImpl implements XAResource {
+    @Override
+    public void commit(Xid xid, boolean b) throws XAException {
+    }
+
+    @Override
+    public void end(Xid xid, int i) throws XAException {
+    }
+
+    @Override
+    public void forget(Xid xid) throws XAException {
+    }
+
+    @Override
+    public int getTransactionTimeout() throws XAException {
+        return 0;
+    }
+
+    @Override
+    public boolean isSameRM(XAResource xaResource) throws XAException {
+        return false;
+    }
+
+    @Override
+    public int prepare(Xid xid) throws XAException {
+        return 0;
+    }
+
+    @Override
+    public Xid[] recover(int i) throws XAException {
+        return new Xid[0];
+    }
+
+    @Override
+    public void rollback(Xid xid) throws XAException {
+    }
+
+    @Override
+    public boolean setTransactionTimeout(int i) throws XAException {
+        return false;
+    }
+
+    @Override
+    public void start(Xid xid, int i) throws XAException {
+    }
+}
