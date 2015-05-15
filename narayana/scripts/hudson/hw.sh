4c27b474b42ab158598b8a4d9aef2a9a2d67e8e8
Michael Musgrove <mmusgrov@redhat.com>
2015-05-15 18:44:00 +0100

    JBTM-2369 OS X script for collecting hw info

--- narayana/scripts/hudson/hw.sh
+++ narayana/scripts/hudson/hw.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
 
-set -o xtrace
+if [ -x /usr/sbin/system_profiler ]; then
+  sw_vers
+  /usr/sbin/system_profiler
+else
+  set -o xtrace
 
-uname -a
-cat /etc/redhat-release
-java -version
-free -m
-cat /proc/cpuinfo
-cat /proc/meminfo
-cat /proc/devices
-cat /proc/scsi/scsi
-cat /proc/partitions
+  uname -a
+  cat /etc/redhat-release
+  java -version
+  free -m
+  cat /proc/cpuinfo
+  cat /proc/meminfo
+  cat /proc/devices
+  cat /proc/scsi/scsi
+  cat /proc/partitions
 #sudo dmidecode --type 17
 
-lspci
-lsusb
-lsblk
-df
-mount | column -t | grep ext
-
+  lspci
+  lsusb
+  lsblk
+  df
+  mount | column -t | grep ext
+fi
