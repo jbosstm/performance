969d7d0bc5545144b7293646bcd649a2fc233889
Tom Jenkinson <tom.jenkinson@redhat.com>
2017-11-30 12:16:21 +0000

    Updated to check for shell errors

--- scripts/hudson/jenkins.sh
+++ scripts/hudson/jenkins.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+function fatal {
+  comment_on_pull "Tests failed ($BUILD_URL): $1"
+  echo "$1"
+  exit 1
+}
+
 function comment_on_pull
 {
     if [ "$COMMENT_ON_PULL" = "" ]; then return; fi
@@ -28,16 +36,19 @@ PATH=$WORKSPACE/tmp/tools/maven/bin/:$PATH
 comment_on_pull "Started testing this pull request: $BUILD_URL"
 
 GIT_BRANCH=master THREAD_COUNTS="1 24 240 1600" COMPARISON="com.arjuna.ats.jta.xa.performance.*StoreBenchmark.*" COMPARISON_COUNT=4 narayana/scripts/hudson/jenkins.sh
-cp benchmark-output.txt benchmark-store-output.txt
-cp benchmark.png benchmark-store.png
+[ $? = 0 ] || fatal "Store benchmark failed"
+mv benchmark-output.txt benchmark-store-output.txt
+mv benchmark.png benchmark-store.png
 
 GIT_BRANCH=master THREAD_COUNTS="1 24 240 1600" COMPARISON="io.narayana.perf.product.*Comparison.*" COMPARISON_COUNT=5 narayana/scripts/hudson/jenkins.sh
-cp benchmark-output.txt benchmark-comparison-output.txt
-cp benchmark.png benchmark-comparison.png
+[ $? = 0 ] || fatal "Product comparison benchmark failed"
+mv benchmark-output.txt benchmark-comparison-output.txt
+mv benchmark.png benchmark-comparison.png
 
 JVM_ARGS="-DMAX_ERRORS=10" ./narayana/scripts/hudson/benchmark.sh "ArjunaJTA/jta" "org.jboss.narayana.rts.*TxnTest.*" 3
-cp benchmark-output.txt benchmark-rts-output.txt
-cp benchmark.png benchmark-rts.png
+[ $? = 0 ] || fatal "RTS benchmark failed"
+mv benchmark-output.txt benchmark-rts-output.txt
+mv benchmark.png benchmark-rts.png
 
 ./build.sh -f narayana/pom.xml clean package -DskipTests
 wget https://ci.jboss.org/hudson/job/WildFly-latest-master/lastSuccessfulBuild/artifact/dist/target/wildfly-10.x.zip
@@ -46,9 +57,11 @@ unzip wildfly-10.x.zip -d wildfly-dist
 sed -i "s/8080/8180/g" wildfly-dist/*/standalone/configuration/standalone-full.xml
 export JBOSS_HOME=$PWD/$(ls -d wildfly-dist/wildfly-*/ | head -n 1)
 ./build.sh -f comparison/pom.xml clean install -DskipTests
+[ $? = 0 ] || fatal "Transport comparison failed"
 #  mvn -f comparison/rest-at/pom.xml test
 #  mvn -f comparison/ws-at/pom.xml test
 ./build.sh -f comparison/jts/pom.xml test
 ./narayana/scripts/hudson/bm.sh
+[ $? = 0 ] || fatal "BM properties failed"
 
 comment_on_pull "Pull passed: $BUILD_URL"
