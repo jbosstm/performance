9d0d60818443a0a807440017f5f1f39c3da94e7b
Ondra Chaloupka <ochaloup@redhat.com>
2019-01-10 21:55:27 +0100

    [SCRIPTS ONLY] Adding script for rebasing when running pull requests

---
+++ scripts/hudson/performance-rebase.sh
@@ -0,0 +1,44 @@
+export GIT_ACCOUNT=${GIT_ACCOUNT:-jbosstm}
+export GIT_REPO=${GIT_REPO:-performance}
+
+function fatal {
+  comment_on_pull "Tests failed ($BUILD_URL): $1"
+  echo "$1"
+  exit 1
+}
+
+function comment_on_pull
+{
+    if [ "$COMMENT_ON_PULL" = "" ]; then return; fi
+
+    PULL_NUMBER=$(echo $GIT_BRANCH | awk -F 'pull' '{ print $2 }' | awk -F '/' '{ print $2 }')
+    if [ "$PULL_NUMBER" != "" ]
+    then
+        JSON="{ \"body\": \"$1\" }"
+        curl -d "$JSON" -ujbosstm-bot:$BOT_PASSWORD https://api.github.com/repos/$GIT_ACCOUNT/$GIT_REPO/issues/$PULL_NUMBER/comments
+    else
+        echo "Not a pull request, so not commenting"
+    fi
+}
+
+function rebase_performance {
+  echo "Rebasing Performance repository"
+  cd $WORKSPACE
+
+  # Clean up the local repo
+  git rebase --abort
+  rm -rf .git/rebase-apply
+  git clean -f -d -x
+  
+  export BRANCHPOINT=master
+
+  # Update the pull to head  
+  git pull --rebase --ff-only origin $BRANCHPOINT
+
+  if [ $? -ne 0 ]; then
+    fatal "Performance rebase on $BRANCHPOINT failed. Please rebase it manually"
+  fi
+}
+
+rebase_performance "$@"
+exit 0
