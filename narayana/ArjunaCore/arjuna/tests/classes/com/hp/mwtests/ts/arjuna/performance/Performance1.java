9335fff54afe7a388cdd1645da8b1ff0dd380dbb
Mayank Kunwar <msingh722@gmail.com>
2020-03-03 01:55:40 +0530

    Revert "Stray semicolon"

--- narayana/ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/performance/Performance1.java
+++ narayana/ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/performance/Performance1.java
@@ -56,6 +56,8 @@ public class Performance1 {
         private BasicRecord record2 = new BasicRecord();
     }
 
+    ;
+
     static {
         try {
             BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class).setObjectStoreType(VolatileStore.class.getName());
