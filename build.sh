26be44e2f97f1368fa0b461c706ef8b72f9ec05b
Tom Jenkinson <tom.jenkinson@redhat.com>
2022-01-22 14:51:37 +0000

    Updated maven and made some settings

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh
 ### ====================================================================== ###
 ##                                                                          ##
 ##  This is the main entry point for the build system.                      ##
@@ -10,28 +10,57 @@
 
 # $Id: build.sh 105735 2010-06-04 19:45:13Z pgier $
 
+# Create the bpa if you can
+BPA=
+uname | grep Linux >> /dev/null
+if [ "$?" -ne "1" ]; then
+	uname -a | grep x86_64 >> /dev/null
+	if [ "$?" -ne "1" ]; then
+		BPA="-Dbpa=centos54x64"
+	else
+		BPA="-Dbpa=centos55x32"
+	fi
+  # This is required for the upgrade of g++ https://issues.jboss.org/browse/JBTM-1787
+  if [ -f /etc/fedora-release ]
+  then
+	uname -a | grep x86_64 >> /dev/null
+	if [ "$?" -ne "1" ]; then
+	    BPA="-Dbpa=fc18x64"
+	fi
+  fi
+fi
+
+ORIG_WORKING_DIR=`pwd`
 PROGNAME=`basename $0`
 DIRNAME=`dirname $0`
 GREP="grep"
 ROOT="/"
 
-# Ignore user's MAVEN_HOME if it is set
-M2_HOME=""
-MAVEN_HOME=""
+# Ignore user's MAVEN_HOME if it is set (M2_HOME is unsupported since Apache Maven 3.5.0)
+unset M2_HOME
+unset MAVEN_HOME
 
-MAVEN_OPTS="$MAVEN_OPTS -Xmx1024M"
-export MAVEN_OPTS
+JAVA_VERSION=$(java -version 2>&1 | grep "\(java\|openjdk\) version" | cut -d\  -f3 | tr -d '"' | tr -d '[:space:]'| awk -F . '{if ($1==1) print $2; else print $1}')
 
-./tools/download-maven.sh
+if [ $JAVA_VERSION -eq "9" ]; then
+  MAVEN_OPTS="$MAVEN_OPTS --add-modules java.corba"
+  MAVEN_OPTS="$MAVEN_OPTS --add-modules java.xml.bind"
+  MAVEN_OPTS="$MAVEN_OPTS --add-modules java.xml.ws"
+  export MAVEN_OPTS
+fi
 
-#  Default search path for maven.
-MAVEN_SEARCH_PATH="\
-    tools
-    tools/maven \
-    tools/apache/maven \
-    maven"
+if [ -z "$MAVEN_OPTS" ]
+then
+	if [ $JAVA_VERSION -ge "9" ]; then
+		MAVEN_OPTS="$MAVEN_OPTS -Xms1303m -Xmx1303m"
+	else
+    MAVEN_OPTS="$MAVEN_OPTS -Xms1303m -Xmx1303m -XX:MaxPermSize=512m"
+	fi
+	export MAVEN_OPTS
+fi
 
-
+#  Default arguments
+MVN_OPTIONS="-B -s \"${DIRNAME}/.mvn/wrapper/settings.xml\" $BPA"
 
 #  Use the maximum available, or set MAX_FD != -1 to use that
 MAX_FD="maximum"
@@ -75,19 +104,6 @@ source_if_exists() {
     done
 }
 
-find_maven() {
-    search="$*"
-    for d in $search; do
-        MAVEN_HOME="${DIRNAME}/$d"
-        MVN="$MAVEN_HOME/bin/mvn"
-        if [ -x "$MVN" ]; then
-            #  Found.
-            echo $MAVEN_HOME
-            break
-        fi
-    done
-}
-
 #
 #  Main function.
 #
@@ -97,51 +113,29 @@ main() {
 
     #  Increase the maximum file descriptors if we can.
     if [ $cygwin = "false" ]; then
-        if MAX_FD_LIMIT=`ulimit -H -n`; then
+        MVN_OPTIONS="$MVN_OPTIONS -Dorson.jar.location=`pwd`/narayana-tmp/ext/"
+        MAX_FD_LIMIT=`ulimit -H -n`
+        if [ $? -eq 0 ]; then
             if [ "$MAX_FD" = "maximum" -o "$MAX_FD" = "max" ]; then
                 #  Use system's max.
                 MAX_FD="$MAX_FD_LIMIT"
             fi
 
-            if ! ulimit -n $MAX_FD; then
+            ulimit -n $MAX_FD
+            if [ $? -ne 0 ]; then
                 warn "Could not set maximum file descriptor limit: $MAX_FD"
             fi
         else
             warn "Could not query system maximum file descriptor limit: $MAX_FD_LIMIT"
         fi
-    fi
-
-    #  Try the search path.
-    MAVEN_HOME=`find_maven $MAVEN_SEARCH_PATH`
-
-    #  Try looking up to root.
-    if [ "x$MAVEN_HOME" = "x" ]; then
-        target="build"
-        _cwd=`pwd`
-
-        while [ "x$MAVEN_HOME" = "x" ] && [ "$cwd" != "$ROOT" ]; do
-            cd ..
-            cwd=`pwd`
-            MAVEN_HOME=`find_maven $MAVEN_SEARCH_PATH`
-        done
-
-        #  Make sure we get back.
-        cd $_cwd
-
-        if [ "$cwd" != "$ROOT" ]; then
-            found="true"
-        fi
-
-        #  Complain if we did not find anything.
-        if [ "$found" != "true" ]; then
-            die "Could not locate Maven; check \$MVN or \$MAVEN_HOME."
-        fi
+    else
+        MVN_OPTIONS="$MVN_OPTIONS -Dorson.jar.location=`cygpath -w $(pwd)/ext/`"
     fi
 
     #  Make sure we have one.
-    MVN=$MAVEN_HOME/bin/mvn
+    MVN="${DIRNAME}/mvnw"
     if [ ! -x "$MVN" ]; then
-        die "Maven file is not executable: $MVN"
+        die "Maven binary is not executable: $MVN"
     fi
 
     #  Need to specify planet57/buildmagic protocol handler package.
@@ -154,17 +148,12 @@ main() {
     #  to be in the same directory as build.xml.
     cd $DIRNAME
 
-    #  Add default settings before calling maven.
-    MVN_SETTINGS_XML_ARGS="-s tools/maven/conf/settings.xml"
     MVN_GOAL="";
     ADDIT_PARAMS="";
     #  For each parameter, check for testsuite directives.
     for param in "$@" ; do
         case $param in
-            ## -s .../settings.xml - don't use our own.
-            -s)      MVN_SETTINGS_XML_ARGS="";   ADDIT_PARAMS="$ADDIT_PARAMS $param";;
             -*)      ADDIT_PARAMS="$ADDIT_PARAMS '$param'";;
-            help*)   MVN_GOAL="$MVN_GOAL$param ";;
             clean)   MVN_GOAL="$MVN_GOAL$param ";;
             test)    MVN_GOAL="$MVN_GOAL$param ";;
             install) MVN_GOAL="$MVN_GOAL$param ";;
@@ -177,18 +166,18 @@ main() {
     if [ -z "$MVN_GOAL" ]; then MVN_GOAL="install"; fi
 
     #  Export some stuff for maven.
-    export MVN MAVEN_HOME MVN_OPTS MVN_GOAL
+    export MVN MVN_OPTS MVN_GOAL
 
-    # The default arguments.  `mvn -s ...` will override this.
-    MVN_ARGS=${MVN_ARGS:-"$MVN_SETTINGS_XML_ARGS"};
+    echo "$MVN $MVN_OPTIONS $MVN_GOAL $ADDIT_PARAMS"
 
-    echo "$MVN $MVN_ARGS $MVN_GOAL $ADDIT_PARAMS"
+    # workaround in case 'mvn -f' is not supported
+    if [ "$PRESERVE_WORKING_DIR" = "true" ]; then cd "$ORIG_WORKING_DIR"; fi
 
     #  Execute in debug mode, or simply execute.
     if [ "x$MVN_DEBUG" != "x" ]; then
-        eval /bin/sh -x $MVN $MVN_ARGS -Dorson.jar.location=`pwd`/narayana-tmp/ext/ $MVN_GOAL $ADDIT_PARAMS
+        eval /bin/sh -x $MVN $MVN_OPTIONS $MVN_GOAL $ADDIT_PARAMS
     else
-        eval exec       $MVN $MVN_ARGS -Dorson.jar.location=`pwd`/narayana-tmp/ext/ $MVN_GOAL $ADDIT_PARAMS
+        eval exec $MVN $MVN_OPTIONS $MVN_GOAL $ADDIT_PARAMS
     fi
 }
 
