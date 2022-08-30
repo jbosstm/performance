dbf610128d0f5ec3d35de2ec8f64566bb5c55da9
Marco Sappé Griot <msappegr@redhat.com>
2022-08-30 15:54:57 +0200

    [JBTM-3640] Remove and replace Jacorb in performance repo (product)

--- product/src/test/java/com/arjuna/ats/tools/perftest/task/ProductPerformanceTest.java
+++ product/src/test/java/com/arjuna/ats/tools/perftest/task/ProductPerformanceTest.java
@@ -159,8 +159,7 @@ public class ProductPerformanceTest
         output.close();
     }
 
-    //@Test
-    //Jacorb does not work with JDK11 (java.lang.NoClassDefFoundError: javax/rmi/CORBA/Stub), Resolve this with JBTM-3640
+    @Test
     public void testProduct() throws Exception {
         TaskResult results[] = new TaskResult[products.length];
 
